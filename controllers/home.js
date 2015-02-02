863643af7ccb3da172c5045f68279ad49d70d055
Justin <jxm262@gmail.com>
2015-02-02 11:51:17 -0500

    fixing notification for password reset on home page

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,9 @@
  * Home page.
  */
 exports.index = function(req, res) {
+  console.log(req.session.flash);
   res.render('home', {
-    title: 'Home'
+    title: 'Home',
+    flash: req.session.flash
   });
 };
